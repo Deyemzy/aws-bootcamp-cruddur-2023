775e194096a3bcc7061b3c06cda86d2a1218f263
Deyemzy <adedejiadeyemid@gmail.com>
2023-03-21 02:52:33 +0000

    cognito

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -18,7 +18,7 @@ import {
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
-  "AWS_PROJECT_REGION": process.env.REACT_AWS_PROJECT_REGION,
+  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
   "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
   "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
   "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
